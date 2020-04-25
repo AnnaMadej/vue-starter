528e771ccc3da1d0a7a3d85d2e85de51ecca9bbb
Anna Madej <aniamadej123@gmail.com>
2020-04-25 14:02:51 +0200

    logging and milligram

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="app">
-    <h1>Twój e-mail to: {{email}}</h1>
-    <div v-if="email.length < 10">Ale masz krótki adres!</div>
-    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-    <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
-    <input type = "text" v-model="email" /><button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+    <h1>Witaj w systemie zapisów na zajęcia!</h1>
+    <div v-if="!logged">
+      Zaloguj sie emailem:
+    <input type = "email" v-model="email" /><button @click="loginUser()">Wchodzę!</button>
+    </div>
+    <div v-else>
+      Witaj, {{email}} !
+    </div>
+
   </div>
 </template>
 
 <script>
 
+  import "milligram";
+
   export default {
     data() {
       return {
+        logged: false,
         email: ''
       }
     },
@@ -20,6 +27,9 @@
     methods: {
       alertMyEmail() {
         alert(this.email);
+      },
+      loginUser() {
+        this.logged = true;
       }
     }
   }
